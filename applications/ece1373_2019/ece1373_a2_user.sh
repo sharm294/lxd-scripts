24c70046326b17981f64192d2b8fc3f1c7103207
Varun Sharma <varuns.sharma@mail.utoronto.ca>
2019-02-25 10:15:05 -0500

    Fix permissions on caffe folder

--- applications/ece1373_2019/ece1373_a2_user.sh
+++ applications/ece1373_2019/ece1373_a2_user.sh
@@ -8,8 +8,10 @@ homeDir=/home/$username
 
 lxc exec $agent:$container -- git clone https://github.com/UofT-HPRC/ECE1373_assignment2.git $homeDir/ECE1373_assignment2/
 lxc file push tarballs/static_routed_v1.dcp $agent:${container}$homeDir/ECE1373_assignment2/8v3_shell/
+lxc exec $agent:$container -- mkdir -p /opt/util/program
 lxc file push tarballs/v1_clear.bit $agent:${container}/opt/util/program/
 lxc exec $agent:$container -- chown -R root:$DEFAULT_NON_SUDO_GROUP /opt/util/
 lxc exec $agent:$container -- chown -R root:$DEFAULT_NON_SUDO_GROUP /opt/caffe
 lxc exec $agent:$container -- chown -R $username:$username $homeDir/
 lxc exec $agent:$container -- chmod -R o-rx $homeDir/
+lxc exec $agent:$container -- chmod 755 /opt/caffe/examples/imagenet/ilsvrc12_train_224_lmdb
